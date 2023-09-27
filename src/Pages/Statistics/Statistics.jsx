dab2c606b41c8ea6f8d5007c4507257105c41f81
Amin7775 <amin.dpi24@gmail.com>
2023-09-27 19:44:51 +0600

    Fixed Pie Chart problem when the donation is 0

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,27 +1,79 @@
+import { useEffect, useState } from "react";
 import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip, Legend } from "recharts";
 
 const Statistics = () => {
   // const [pieData,setPieData]=useState();
+  const [count , setCount] = useState([]);
   let total = 12;
-  let donated = JSON.parse(localStorage.getItem("donationDetails"));
-  let count = donated.length;
-  // total=total-count;
+  let donatedJson = JSON.parse(localStorage.getItem("donationDetails"));
+  let donated = donatedJson ||  [];
 
+  useEffect(()=>{
+    setCount(donated.length);
+  },[donated.length])
+  
+  // total=total-count;
+  // Calculation
   let countPercent = parseFloat(((100 * count) / total).toFixed(1));
   console.log(countPercent);
   let totalPercent = 100 - countPercent;
 
-  const colors = ["#FF444A", "#00C49F"];
+
+  // PIE CHART ESSENTIALS
+  const COLORS = ["#FF444A", "#00C49F"];
 
   const data = [
     { name: "Total Donation", value: totalPercent },
     { name: "Your Donation", value: countPercent },
   ];
   
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  
+    return (
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
   return (
     <div className="flex h-[80vh] items-center justify-center">
-      <div style={{ width: "100%", height: 700 }}>
-        <ResponsiveContainer>
+      <div style={{ width: "100%", height: "100%" }}>
+        
+        <ResponsiveContainer width="100%" height="100%">
+        <PieChart width={400} height={800}>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={'80vw'}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <Legend iconType="plainline" iconSize={100} />
+        </PieChart>
+      </ResponsiveContainer>
+      </div>
+    </div>
+  );
+};
+
+export default Statistics;
+
+
+
+
+{/* <ResponsiveContainer>
           <PieChart>
             <Pie dataKey="value" data={data} fill="#8884d8" label>
               {data.map((entry, index) => (
@@ -29,12 +81,6 @@ const Statistics = () => {
               ))}
             </Pie>
             <Tooltip></Tooltip>
-            <Legend iconType="plainline" iconSize={100} />
+            
           </PieChart>
-        </ResponsiveContainer>
-      </div>
-    </div>
-  );
-};
-
-export default Statistics;
+        </ResponsiveContainer> */}
